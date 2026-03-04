ea8c97eb4409d8c08e3883346f2e5c9424e9c9b2
Bishwadeep Dhali <dhalibishwadeep@gmail.com>
2026-03-04 15:06:28 +0530

    Added the sales.py file

---
+++ sales.py
@@ -0,0 +1,59 @@
+n = int(input('Enter the no of days of sales recorded: '))
+sales = []
+days = []
+c = 1
+if n == 0:
+    print('No sales data recorded!')
+    exit()
+elif n<0:
+    print('Invalid input!')
+    exit()
+else:
+    while c <= n:
+         try:
+             a = int(input(f'Enter the sales for day {c}: '))
+             if a >= 0:
+               sales.append(a)
+               days.append(f'Day {c}')
+               c += 1 
+             else:
+                print("Invalid input! Sales cannot be negative. Try again.")
+        
+         except ValueError:
+             print("Invalid input! Please enter a number.")
+  
+
+
+    print('Highest sales: ',max(sales))
+    print('Day of highest sales: Day ', sales.index(max(sales))+1)
+    print('Lowest sales: ',min(sales))
+    print('Day of lowest sales: Day ', sales.index(min(sales))+1)
+    print('Average sales: ', sum(sales)/len(sales))
+
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(1, 2, figsize=(14, 4))  
+    ax[0].plot(days, sales, marker='o', color='red', label='Sales per day')
+    ax[0].set_title('Line Graph')
+    ax[0].set_xlabel('Days')
+    ax[0].set_ylabel('Sales (units)')
+    ax[0].grid(True)
+    ax[0].legend()
+
+
+    ax[1].bar(days, sales, color='skyblue', label='Sales per day')
+    ax[1].set_title('Bar Graph')
+    ax[1].set_xlabel('Days')
+    ax[1].set_ylabel('Sales (units)')
+    ax[1].grid(axis='y', linestyle='--', alpha=0.2)
+    ax[1].legend()
+
+    for i, v in enumerate(sales):
+        ax[1].text(i, v + 0.5, str(v), ha='center')
+  
+    plt.tight_layout()
+    plt.show()
+
+   
+
+
